f28f43c58ac66adfa74e7b1a51271533ce5c4a2d
Fernando Davila <fernandodavilam@hotmail.com>
2025-10-01 19:45:00 -0400

    Trigger Vercel deployment with test commit

--- server/api/index.js
+++ server/api/index.js
@@ -113,7 +113,7 @@ app.use((err, req, res, next) => {
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri, {
   tls: true,
-  serverSelectionTimeoutMS: 7000,
+  serverSelectionTimeoutMS: 5000,
 });
 
 async function startServer() {
